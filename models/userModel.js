b17884aa28c9e9192cbcd1e1b9fa56d340fa34bd
mohamed yasser <yassermohamed7399@gmail.com>
2025-02-11 00:52:48 +0200

    slugify name

--- models/userModel.js
+++ models/userModel.js
@@ -1,57 +1,66 @@
-const mongoose = require('mongoose');
-const validator = require('validator');
+const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,  
-        required: [true, 'Name is required'],  
-        validate: {
-            validator: function (value) { 
-                return validator.isAlpha(value, 'en-US', { ignore: ' ' }); 
-            },
-            message: 'Name must contain only letters!'   
-        }
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+    validate: {
+      validator: function (value) {
+        return validator.isAlpha(value, "en-US", { ignore: " " });
+      },
+      message: "Name must contain only letters!",
     },
-    email: {
-        type: String,  
-        required: [true, 'Email is required'],
-        unique: true, 
-        lowercase: true, 
-        validate: {
-            validator: function (value) {
-                return validator.isEmail(value);
-            },
-            message: 'Provide a valid Email'
-        }
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    lowercase: true,
+    validate: {
+      validator: function (value) {
+        return validator.isEmail(value);
+      },
+      message: "Provide a valid Email",
     },
-    phone: {
-        type: String,
-        validate: {
-            validator: function (value) {
-                return validator.isMobilePhone(value, 'any');       
-            },
-            message: 'Provide a valid phone number'
-        }
+  },
+  phone: {
+    type: String,
+    validate: {
+      validator: function (value) {
+        return validator.isMobilePhone(value, "any");
+      },
+      message: "Provide a valid phone number",
     },
-    password: {
-        type: String,
-        required: [true, "Password is required"],
-        minlength: [6, "Too short password"],
-        select: false          
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minlength: [6, "Too short password"],
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "Please confirm your password"],
+    validate: {
+      validator: function (value) {
+        return value === this.password;
+      },
+      message: "Passwords are not the same",
     },
-    passwordConfirm: {
-        type: String,
-        required: [true, "Please confirm your password"],
-        validate: {
-            validator: function (value) {
-                return value === this.password;
-            },
-            message: "Passwords are not the same"
-        },
-        select: false  
-    }
+    select: false,
+  },
+  slug: {
+    type: String,
+    lowercase: true,
+  },
 });
 
-const User = mongoose.model('User', userSchema);
+userSchema.pre("save", function(next){
+  this.slug = slugify(this.name);
+  next();
+})
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
