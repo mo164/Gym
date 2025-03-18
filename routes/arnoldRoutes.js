e1c3c78fee75226ce4cc8769fbfa3060ecf0c993
mohamed yasser <yassermohamed7399@gmail.com>
2025-03-18 17:12:59 +0200

    arnold system routes

---
+++ routes/arnoldRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const arnoldController = require("../controllers/pushPullController");
+const router = express.Router();
+
+router.route('/')
+.post(arnoldController.addtrainingProramme)
+.get(arnoldController.getAlltrainingProrammes)
+
+router.route('/:id')
+.delete(arnoldController.deleteSpecificDay)
+.patch(arnoldController.updateTrainingProrammes)
+.get(arnoldController.getSpecificDay)
+
+module.exports = router;
